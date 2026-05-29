4ebfde1bae050f6d2e157656d3857d4e7b531696
Daniel Fernandes <daniel.fernandes.1540295@sga.pucminas.br>
2026-05-29 07:51:51 -0300

    Adicionado middleware para CORS

--- main.go
+++ main.go
@@ -3,13 +3,14 @@ package main
 import (
 	"example.com/dad_midia/db"
 	"example.com/dad_midia/routes"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	db.InitDB()
 	server := gin.Default()
-
+	server.Use(cors.Default()) // default = todas as origens permitidas
 	routes.RegisterRoutes(server)
 
 	server.Run(":8080") // 127.0.0.1:8080
